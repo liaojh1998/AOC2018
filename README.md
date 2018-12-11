14610e3f380a2c20a8b187278aba382ba1fc99fb
Jie Hao Liao <liaojh1998@gmail.com>
2018-12-11 17:48:27 -0600

    :sparkles: Added Day 1 Part 1 Problem: - Circle CI cannot work functionally with Maven (for now, and I desire for an alternative testing environment).
    Solution:
    - Split Java and Python unit testing.
    - Use Travis CI with their already set up Maven testing.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AOC2018
-This repository is purely experimental around understanding CircleCI and different programming languages.
+This repository is purely experimental around understanding ~~CircleCI~~ Travis CI and different programming languages.
+
+CircleCI doesn't seem to be functional for Maven projects so I'll be using Travis CI.
---
+++ day1/part1/ChronalCalibration.java
@@ -0,0 +1,56 @@
+package part1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ChronalCalibration {
+
+    public static int sum(int[] arr) {
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++)
+            ans += arr[i];
+        return ans;
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length < 1)
+            throw new IOException("Need to specify an input file path.");
+
+        // Read frequency changes and convert them to integers.
+        BufferedReader fin = new BufferedReader(new FileReader(new File(args[0])));
+        try {
+            ArrayList<Integer> buffer = new ArrayList<Integer>();
+        
+            String str;
+            while ((str = fin.readLine()) != null)
+                if (str.length() < 1)
+                    throw new IOException("Detected invalid input: " + str + ".");
+                else if (str.charAt(0) == '+')
+                    buffer.add(Integer.valueOf(str.substring(1)));
+                else
+                    buffer.add(Integer.valueOf(str));
+        
+            int[] arr = new int[buffer.size()];
+            for (int i = 0; i < buffer.size(); i++)
+                arr[i] = buffer.get(i);
+
+            // Write the summation of frequency changes.
+            if (args.length == 2) {
+                BufferedWriter fout = new BufferedWriter(new FileWriter(new File(args[1])));
+                try {
+                    fout.write(String.valueOf(sum(arr)) + "\n");
+                } finally {
+                    fout.close();
+                }
+            } else
+                System.out.println(sum(arr));
+        } finally {
+            fin.close();
+        }
+    }
+}
