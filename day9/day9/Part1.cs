59c39f9220d0cda4172609259a5a0182f6262dea
liaojh1998 <liaojh1998@gmail.com>
2019-01-10 13:47:22 -0600

    :sparkles: Day 9 Part 1 Completed! Problem: - Simulate a procedure for the highest score.
    Solution:
    - Just do the simulation brute force. No need to do math.

---
+++ day9/day9/Part1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace part1
+{
+    public class MarbleMania
+    {
+        public static int Main(string[] args)
+        {
+            if (args.Length < 1 || args.Length > 2)
+            {
+                Console.WriteLine("usage: Part1 <input file> <optional output file>");
+                return 1;
+            }
+
+            StreamReader input = new StreamReader(args[0]);
+            string line = input.ReadLine();
+            input.Close();
+
+            string[] values = line.Split(' ');
+            int players = int.Parse(values[0]);
+            int marbles = int.Parse(values[values.Length - 2]);
+
+            if (args.Length == 2)
+            {
+                StreamWriter output = new StreamWriter(args[1]);
+                output.WriteLine(Simulate(players, marbles));
+                output.Close();
+            }
+            else
+            {
+                Console.WriteLine(Simulate(players, marbles));
+            }
+            return 0;
+        }
+
+        public static int Simulate(int players, int marbles_count)
+        {
+            LinkedList<int> marbles = new LinkedList<int>();
+            if (players <= 0)
+                return 0;
+            int[] player_scores = new int[players];
+
+            int player = 0;
+            LinkedListNode<int> marble = marbles.AddFirst(0);
+            for (int i = 1; i <= marbles_count; i++)
+            {
+                if (i % 23 == 0)
+                {
+                    player_scores[player] += i;
+                    for (int j = 0; j < 6; j++)
+                    {
+                        if (marble.Previous == null)
+                        {
+                            marble = marbles.Last;
+                        }
+                        else
+                        {
+                            marble = marble.Previous;
+                        }
+                    }
+                    if (marble.Previous == null)
+                    {
+                        player_scores[player] += marbles.First.Value;
+                        marbles.RemoveFirst();
+                    }
+                    else
+                    {
+                        player_scores[player] += marble.Previous.Value;
+                        marbles.Remove(marble.Previous);
+                    }
+                }
+                else
+                {
+                    if (marble.Next == null)
+                    {
+                        marble = marbles.AddAfter(marbles.First, i);
+                    }
+                    else
+                    {
+                        marble = marbles.AddAfter(marble.Next, i);
+                    }
+                }
+                player = (player + 1) % players;
+            }
+
+            return player_scores.Max();
+        }
+    }
+}
---
+++ day9/day9test/Part1Test.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace part1
+{
+    [TestClass]
+    public class Part1UnitTests
+    {
+        [TestMethod]
+        public void TestSimulateShouldHandleNoPlayersCase()
+        {
+            int expectedSimulation = 0;
+            int actualSimulation = MarbleMania.Simulate(0, 10);
+            Assert.AreEqual(expectedSimulation, actualSimulation);
+        }
+
+        [TestMethod]
+        public void TestSimulateShouldHandleNoMarblesCase()
+        {
+            int expectedSimulation = 0;
+            int actualSimulation = MarbleMania.Simulate(10, 0);
+            Assert.AreEqual(expectedSimulation, actualSimulation);
+        }
+
+        [TestMethod]
+        public void TestSimulateShouldCirculateOnceButReturnNoHighScore()
+        {
+            int expectedSimulation = 0;
+            int actualSimulation = MarbleMania.Simulate(1, 10);
+            Assert.AreEqual(expectedSimulation, actualSimulation);
+        }
+
+        [TestMethod]
+        public void TestSimulateShouldHandleExampleCaseForOneLoopWithActualHighScore()
+        {
+            int expectedSimulation = 32;
+            int actualSimulation = MarbleMania.Simulate(9, 25);
+            Assert.AreEqual(expectedSimulation, actualSimulation);
+        }
+
+        [TestMethod]
+        public void TestSimulateShouldHandleExampleGeneralCase1()
+        {
+            int expectedSimulation = 8317;
+            int actualSimulation = MarbleMania.Simulate(10, 1618);
+            Assert.AreEqual(expectedSimulation, actualSimulation);
+        }
+
+        [TestMethod]
+        public void TestSimulateShouldHandleExampleGeneralCase2()
+        {
+            int expectedSimulation = 146373;
+            int actualSimulation = MarbleMania.Simulate(13, 7999);
+            Assert.AreEqual(expectedSimulation, actualSimulation);
+        }
+
+        [TestMethod]
+        public void TestSimulateShouldHandleExampleGeneralCase3()
+        {
+            int expectedSimulation = 2764;
+            int actualSimulation = MarbleMania.Simulate(17, 1104);
+            Assert.AreEqual(expectedSimulation, actualSimulation);
+        }
+
+        [TestMethod]
+        public void TestSimulateShouldHandleExampleGeneralCase4()
+        {
+            int expectedSimulation = 54718;
+            int actualSimulation = MarbleMania.Simulate(21, 6111);
+            Assert.AreEqual(expectedSimulation, actualSimulation);
+        }
+
+        [TestMethod]
+        public void TestSimulateShouldHandleExampleGeneralCase5()
+        {
+            int expectedSimulation = 37305;
+            int actualSimulation = MarbleMania.Simulate(30, 5807);
+            Assert.AreEqual(expectedSimulation, actualSimulation);
+        }
+    }
+}
